5ea1a354e65cdc2186bd2dedc527f6f8023698e3
Your Name <your@gmail.com>
2025-05-30 05:04:04 +0300

    model post migrate seeder super-user

---
+++ database/migrations/2025_05_30_015533_create_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Название поста
+            $table->text('text'); // Текст поста
+            $table->timestamps();
+            $table->softDeletes(); // Добавить столбец deleted_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,22 @@
         </div>
     </div>
 
-
+    <div class="container mt-6">
+        <div class="row">
+            <div class="col-md-12">
+                <button type="button" class="btn btn-outline-success mb-3">Add post</button>
+                <div class="card">
+                    <h5 class="card-header">card 1</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">title card 1</h5>
+                        <p class="card-text mb-3">Text card 1</p>
+                        <a href="#" class="btn btn-outline-primary">Edit</a>
+                        <a href="#" class="btn btn-outline-danger">Delete</a>
+                        <button type="submit" class="btn btn-outline-warning">Restore</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-app-layout>
